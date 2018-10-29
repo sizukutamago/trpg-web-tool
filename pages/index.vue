2a87b05ba5193f206e90706bd14a1e1c91747a08
Ivory95 <ivory17a@yahoo.co.jp>
2018-10-29 19:06:56 +0900

    test
    とりあえず上げてみます。

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,61 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        trpg-web-tool
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+<div class="wrapper">
+  <header>
+    <h1><a href="">TITLE</a></h1>
+    <ul class="menu">
+      <li><a href="">ダイスロール</a></li>
+      <li><a href="">キャラシート作成</a></li>
+    </ul>
+  </header>
+  <div class="main">
+    <div class="c1">
+    <div class="title">
+      <h2><a href="">TITLE</a></h2>
+      <p>TRPGで使用するダイスロールとキャラシート作成を公開しています。良かったら使ってみてください。後々更なる機能を追加予定。</p>
+    </div>
+    <ul class="m_menu">
+      <li><a href="">ダイスロール</a></li>
+      <li><a href="">キャラシート作成</a></li>
+    </ul>
+    </div>  <!-- c1 end -->
+    <div class="news">
+      <h2 class="n_header"> NEWS </h2>
+      <div class="news_wrapper">
+        <div class="news_content">
+          <div class="nc_header">
+            <h3 class="n_title">news TITLE</h3>
+            <span class="n_category">category,category</span>
+            <span class="n_time">create time</span>
+          </div>
+          <div class="n_description">
+            <p>更新や連絡などアレ。</p>
+          </div>
+        </div>
+        <div class="news_content">
+          <div class="nc_header">
+            <h3 class="n_title">news TITLE</h3>
+            <span class="n_category">category,category</span>
+            <span class="n_time">create time</span>
+          </div>
+          <div class="n_description">
+            <p>更新や連絡などアレ。</p>
+          </div>
+        </div>
+        <div class="news_content">
+          <div class="nc_header">
+            <h3 class="n_title">news TITLE</h3>
+            <span class="n_category">category,category</span>
+            <span class="n_time">create time</span>
+          </div>
+          <div class="n_description">
+            <p>更新や連絡などアレ。</p>
+          </div>
+        </div>
       </div>
     </div>
-  </section>
+  </div>
+  <footer>
+    create sizukutamago Ivory 2018
+  </footer>
+</div>
 </template>
-
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
